f709d02a966f6495ba18d114023fea455e8b19a9
mjsong07 <182254440@qq.com>
2024-07-23 14:52:49 +0800

    init

---
+++ application/controllers/test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 测试
+ */
+
+use Model\AclModel;
+use Model\AreaModel;
+use Model\MenuModel;
+use Mvc\AbstractController;
+
+class ResourceController extends AbstractController
+{ 
+
+
+    /**
+     * 测试vue响应式
+     * @throws Exception
+     */
+    public function testvue3Action()
+    { 
+    
+    } 
+
+    /**
+     * 测试 增删改查
+     * @throws Exception
+     */
+    public function listAction()
+    { 
+        
+    }
+
+}
